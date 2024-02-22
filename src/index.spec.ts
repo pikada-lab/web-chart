e01d3e0fb8416873259bb6c7fcc47db90e91c476
Dzhigurda Anton <info@pikada-lab.ru>
2024-02-23 00:17:22 +0300

    init

---
+++ src/index.spec.ts
@@ -0,0 +1,174 @@
+import { Task, TaskEvent, TypeEvent, WebChart } from "./index";
+
+describe("Модель сетевого графика", () => {
+  it("Работа должна создаваться с названием и идентификатором", () => {
+    Task.LAST_ID = 0;
+    expect(new Task("Работа 1").id).toBe(1);
+  });
+  it("Событие должно содержать корректный тип: Начало", () => {
+    const sut = new TaskEvent(TypeEvent.START);
+    expect(sut.isStart()).toBeTruthy();
+    expect(sut.isDefault()).toBeFalsy();
+    expect(sut.isEnd()).toBeFalsy();
+  });
+
+  it("Событие должно содержать корректный тип: Окончание", () => {
+    const sut = new TaskEvent(TypeEvent.END);
+    expect(sut.isStart()).toBeFalsy();
+    expect(sut.isDefault()).toBeFalsy();
+    expect(sut.isEnd()).toBeTruthy();
+  });
+
+  it("Событие должно содержать корректный тип: По умолчанию", () => {
+    const sut = new TaskEvent();
+    expect(sut.isStart()).toBeFalsy();
+    expect(sut.isDefault()).toBeTruthy();
+    expect(sut.isEnd()).toBeFalsy();
+  });
+
+  it("Работа должна корректно соединяться с событиями", () => {
+    TaskEvent.LAST_ID = 0;
+    const start = new TaskEvent(TypeEvent.START);
+    const end = new TaskEvent(TypeEvent.END);
+    const task = new Task("Работа 2");
+
+    const connect = task.connect(start, end);
+
+    expect(connect).toBeTruthy();
+    expect(task.hasConnect).toBeTruthy();
+    expect(task.key).toBe(`(1,2)`);
+    expect(start.next()).toBe(task);
+    expect(task.next()).toBe(end);
+  });
+
+  it("Должен создаваться простой сетевой график", () => {
+    const sut = new WebChart("Сетевой график");
+    const start = new TaskEvent(TypeEvent.START);
+    const end = new TaskEvent(TypeEvent.END);
+    const task = new Task("Работа 3");
+    const connect = task.connect(start, end);
+
+    sut.addEvent(start);
+    sut.addEvent(end);
+    sut.addTask(task);
+
+    expect(connect).toBeTruthy();
+    expect(sut.check()).toBeTruthy();
+  });
+
+  it("Если у сетевого графика нет начала, он некорректный", () => {
+    const sut = new WebChart("Сетевой график");
+    expect(sut.check()).toBeFalsy();
+  });
+
+  it("Если у сетевого графика нет конца, он некорректный", () => {
+    const sut = new WebChart("Сетевой график");
+    const start = new TaskEvent(TypeEvent.START);
+    sut.addEvent(start);
+    expect(sut.check()).toBeFalsy();
+  });
+
+  it("Если из начала нельзя попасть в конец, то график не корректный", () => {
+    const sut = new WebChart("Сетевой график");
+    const start = new TaskEvent(TypeEvent.START);
+    const end = new TaskEvent(TypeEvent.END);
+    const task = new Task("Работа 3");
+
+    sut.addEvent(start);
+    sut.addEvent(end);
+    sut.addTask(task);
+
+    expect(sut.check()).toBeFalsy();
+  });
+
+  it("Если есть тупиковое событие, то график не корректный", () => {
+    const sut = new WebChart("Сетевой график");
+    TaskEvent.LAST_ID = 0;
+    const start = new TaskEvent(TypeEvent.START);
+    const end = new TaskEvent(TypeEvent.END);
+    const middle = new TaskEvent();
+    const impasse = new TaskEvent();
+    Task.LAST_ID = 0;
+    const task1 = new Task("Работа 1");
+    const task2 = new Task("Работа 2");
+    const task3 = new Task("Работа 3");
+    task1.connect(start, middle);
+    task2.connect(middle, end);
+    task3.connect(middle, impasse);
+
+    sut.addEvent(start);
+    sut.addEvent(end);
+    sut.addEvent(middle);
+    sut.addEvent(impasse);
+    sut.addTask(task1);
+    sut.addTask(task2);
+    sut.addTask(task3);
+
+    expect(sut.check()).toBeFalsy();
+  });
+  
+  it("Если есть хвостовое событие, то график не корректный", () => {
+    const sut = new WebChart("Сетевой график");
+    TaskEvent.LAST_ID = 0;
+    const start = new TaskEvent(TypeEvent.START);
+    const end = new TaskEvent(TypeEvent.END);
+    const middle = new TaskEvent();
+    const tail = new TaskEvent();
+    Task.LAST_ID = 0;
+    const task1 = new Task("Работа 1");
+    const task2 = new Task("Работа 2");
+    const task3 = new Task("Работа 3");
+    task1.connect(start, middle);
+    task2.connect(middle, end);
+    task3.connect(tail, middle);
+
+    sut.addEvent(start);
+    sut.addEvent(end);
+    sut.addEvent(middle);
+    sut.addEvent(tail);
+    sut.addTask(task1);
+    sut.addTask(task2);
+    sut.addTask(task3);
+
+    expect(sut.check()).toBeFalsy();
+  });
+  
+  it("Если есть замкнутый контур, то график не корректный", () => {
+    const sut = new WebChart("Сетевой график");
+    TaskEvent.LAST_ID = 0;
+    const start = new TaskEvent(TypeEvent.START);
+    const end = new TaskEvent(TypeEvent.END);
+    const middle1 = new TaskEvent();
+    const middle2 = new TaskEvent();
+    const middle3 = new TaskEvent();
+    const middle4 = new TaskEvent(); 
+    Task.LAST_ID = 0;
+    const task1 = new Task("Работа 1");
+    const task2 = new Task("Работа 2");
+    const task3 = new Task("Работа 3");
+    const task4 = new Task("Работа 4");
+    const task5 = new Task("Работа 5");
+    const task6 = new Task("Работа 6");
+    task1.connect(start, middle1);
+    task2.connect(middle1, middle2);
+    task3.connect(middle2, middle3);
+    task4.connect(middle3, middle4);
+    task5.connect(middle4, middle1);
+    task6.connect(middle1, end);
+
+    sut.addEvent(start);
+    sut.addEvent(end);
+    sut.addEvent(middle1);
+    sut.addEvent(middle2);
+    sut.addEvent(middle3);
+    sut.addEvent(middle4);
+    sut.addTask(task1);
+    sut.addTask(task2);
+    sut.addTask(task3);
+    sut.addTask(task4);
+    sut.addTask(task5);
+    sut.addTask(task6);
+
+    expect(sut.check()).toBeFalsy();
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,217 @@
+export enum TaskType {
+  HARD = 1,
+  FICTION = 2,
+}
+
+/**
+ * Работа
+ * ---->
+ */
+export class Task {
+  static LAST_ID = 0;
+  id: number;
+  // Время
+  duration: number;
+  // Граф
+  private start: TaskEvent;
+  private end: TaskEvent;
+  private isConnect: boolean = false;
+
+  get hasConnect(): boolean {
+    return this.isConnect;
+  }
+
+  get key(): string {
+    return `(${this.start.getId()},${this.end.getId()})`;
+  }
+
+  constructor(
+    public readonly name: string,
+    public readonly type: TaskType = TaskType.HARD
+  ) {
+    this.id = ++Task.LAST_ID;
+  }
+
+  next(): TaskEvent | null {
+    if (!this.isConnect) {
+      return null;
+    }
+    return this.end;
+  }
+
+  connect(start: TaskEvent, end: TaskEvent): boolean {
+    if (this.isConnect) {
+      return false;
+    }
+    if (start.isEnd()) {
+      return false;
+    }
+    if (end.isStart()) {
+      return false;
+    }
+    this.start = start;
+    start.setOutput(this);
+    this.end = end;
+    end.setInput(this);
+    this.isConnect = true;
+    return true;
+  }
+}
+
+export enum TypeEvent {
+  START = 1,
+  DEFAULT = 2,
+  END = 3,
+}
+
+/**
+ * Событие
+ * (1)
+ */
+export class TaskEvent {
+  static LAST_ID = 0;
+  private id: number;
+  private readonly input: Task[] = [];
+  private readonly output: Task[] = [];
+
+  constructor(private type: TypeEvent = TypeEvent.DEFAULT) {
+    this.id = ++TaskEvent.LAST_ID;
+  }
+
+  next(): Task | null {
+    if (this.isEnd()) {
+      return null;
+    }
+    if (this.output.length === 0) {
+      return null;
+    }
+    return this.output[0];
+  }
+
+  prev(): Task | null {
+    if (this.isStart()) {
+      return null;
+    }
+    if (this.input.length === 0) {
+      return null;
+    }
+    return this.input[0];
+  }
+
+  getId(): number {
+    return this.id;
+  }
+
+  isEnd(): boolean {
+    return this.type === TypeEvent.END;
+  }
+
+  isStart(): boolean {
+    return this.type === TypeEvent.START;
+  }
+
+  isDefault(): boolean {
+    return this.type === TypeEvent.DEFAULT;
+  }
+
+  setInput(start: Task): void {
+    this.input.push(start);
+  }
+
+  setOutput(end: Task): void {
+    this.output.push(end);
+  }
+}
+
+export class WebChart {
+  private start: TaskEvent | null = null;
+  private end: TaskEvent | null = null;
+  private readonly tasks: Task[] = [];
+  private readonly events: TaskEvent[] = [];
+
+  constructor(private readonly title: string) {}
+
+  addTask(task: Task): void {
+    this.tasks.push(task);
+  }
+
+  addEvent(event: TaskEvent): void {
+    if (event.isStart()) {
+      if (this.start) {
+        return;
+      }
+      this.start = event;
+    }
+    if (event.isEnd()) {
+      if (this.end) {
+        return;
+      }
+      this.end = event;
+    }
+    this.events.push(event);
+  }
+
+  check(debug = false): boolean {
+    if (!this.start) {
+      if (debug) {
+        console.log("Нет начала");
+      }
+      return false;
+    }
+    if (!this.end) {
+      if (debug) {
+        console.log("Нет окончания");
+      }
+      return false;
+    }
+    const unique = new Set();
+    let node: TaskEvent | Task | null = this.start;
+    while (true) {
+      if (node instanceof TaskEvent && node.isEnd()) {
+        break;
+      }
+      if (!node) {
+        if (debug) {
+          console.log("Начало не связанно с окончанием");
+        }
+        return false;
+      }
+      node = node.next();
+      if (unique.has(node)) {
+        if (debug) {
+          console.log("Есть замкнутый контур");
+        }
+        return false;
+      }
+      unique.add(node);
+    }
+    for (let event of this.events) {
+      if (event.isDefault()) {
+        if (event.next() === null) {
+          if (debug) {
+            console.log("Есть тупиковое событие");
+          }
+          return false;
+        }
+        if (event.prev() === null) {
+          if (debug) {
+            console.log("Есть хвостовое событие");
+          }
+          return false;
+        }
+      }
+    }
+    if (debug) {
+      console.log("Корректный сетевой график");
+    }
+    return true;
+  }
+
+  getStart(): TaskEvent | null {
+    return this.start;
+  }
+
+  getEnd(): TaskEvent | null {
+    return this.end;
+  }
+}
